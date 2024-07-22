c81cd1c621ca197e5c086176e4f4e75be567b28c
Gerardo02 <gerardo.jerez@hotmail.com>
2024-07-22 10:15:08 -0700

    dark theme perron

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-
-import './App.css'
 import { ModeToggle } from './components/mode-toggle'
+import { Button } from './components/ui/button'
+import WelcomeCard from './components/welcome'
 
 function App() {
 
   return (
-    <>
+    <div>
       <ModeToggle />
-    </>
+      <WelcomeCard />
+      <Button>Brooooooo</Button>
+    </div>
   )
 }
 
